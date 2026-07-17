c0a7e0730cf664aca4c0fbe30a03a52cf0408b7e
Steve Korshakov <steve@korshakov.com>
2026-07-16 17:21:24 -0700

    Remove unused tools barrel

--- packages/rig/sources/tools/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./claude/index.js";
-export * from "./codex/index.js";
-export * from "./goals/index.js";
-export * from "./pi/index.js";
-export * from "./testing/createJustBashToolHarness.js";
-export * from "./utils/index.js";
